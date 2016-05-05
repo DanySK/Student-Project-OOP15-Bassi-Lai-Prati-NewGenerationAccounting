50e91504196ee29ebf6a41c45acb61508a79cc25
Federico Prati (Pentolo) <federico.prati2@studio.unibo.it>
2016-05-05 22:40:02 +0200

    pulite classi conto e movimento per problemi di merge

--- src/dataModel/Conto.java
+++ src/dataModel/Conto.java
@@ -4,22 +4,13 @@ import dataEnum.Nature;
 
 public class Conto {
 	private Nature natura;
-	private String nome;
-<<<<<<< local
-	private Nature natura;
+	private String nome;	
 	private long avere;
 	private long dare;
 	
 	public Conto(String nome, Nature natura, long dare, long avere) {
-=======
-	private long saldo;
-
-	public Conto(String nome, Nature natura, long saldo) {
->>>>>>> other
 		this.nome = nome;
 		this.natura = natura;
-		this.dare = dare;
-		this.avere = avere;
 	}
 
 	public Nature getNatura() {
@@ -30,35 +21,21 @@ public class Conto {
 		return nome;
 	}
 
-	public long getSaldo() {
-		return saldo;
-	}
-
-<<<<<<< local
-	public Nature getNatura() {
-		return natura;
-	}
-
 	public void setNatura(Nature natura) {
-=======
-	public void setNatura(Nature natura) {
->>>>>>> other
 		this.natura = natura;
 	}
 
-<<<<<<< local
 	public long getAvere() {
 		return avere;
-=======
+	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
->>>>>>> other
 	}
 
 	public void setAvere(long avere) {
 		this.avere = avere;
 	}
-<<<<<<< local
 
 	public long getDare() {
 		return dare;
@@ -67,7 +44,4 @@ public class Conto {
 	public void setDare(long dare) {
 		this.dare = dare;
 	}
-=======
-
->>>>>>> other
 }
--- src/dataModel/Movimento.java
+++ src/dataModel/Movimento.java
@@ -4,54 +4,43 @@ import java.util.Date;
 import java.util.List;
 
 public class Movimento {
-<<<<<<< local
-	Date data;
-	List<Conto> listaConti;
 
+    private Date data;
+    private List<Conto> listaConti;
+    
 	public Movimento(List<Conto> listaConti,Date data) {
 		this.listaConti = listaConti;
 		this.data = data;
 	}
-=======
-    private Date data;
-    private List<String> infoMovimento;
->>>>>>> other
 
     public Movimento(Date data, String nomeConto, String dare, String avere) {
         this.data = data;
-        this.infoMovimento.add(nomeConto);
-        this.infoMovimento.add(dare);
-        this.infoMovimento.add(avere);
+        /*
+        this.listaConti.add(nomeConto);
+        this.listaConti.add(dare);
+        this.listaConti.add(avere);
+        */
     }
 
     public Date getData() {
         return data;
     }
 
-<<<<<<< local
 	public List<Conto> getListaConti() {
 		return listaConti;
 	}
-=======
     public void setData(Date data) {
         this.data = data;
     }
->>>>>>> other
-
-<<<<<<< local
 	public void setListaConti(List<Conto> listaConti) {
 		this.listaConti = listaConti;
 	}
 	
-	
-=======
-    public List<String> getInfoMovimento() {
-        return infoMovimento;
+    public List<Conto> getInfoMovimento() {
+        return listaConti;
     }
 
-    public void setInfoMovimento(List<String> infoMovimento) {
-        this.infoMovimento = infoMovimento;
+    public void setInfoMovimento(List<Conto> infoMovimento) {
+        this.listaConti = infoMovimento;
     }
-
->>>>>>> other
 }
