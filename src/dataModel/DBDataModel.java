9f4d963b462a424080fa511848ec1c9391166baf
Federico Prati (Pentolo) <federico.prati2@studio.unibo.it>
2016-05-15 15:18:26 +0200

    inserita javadoc in dbtdatamodel e sistemate imperfezioni

--- src/dataModel/DBDataModel.java
+++ src/dataModel/DBDataModel.java
@@ -6,14 +6,27 @@ package dataModel;
 import java.util.LinkedList;
 
 /**
+ * il database che conterrà tutte le informazioni del programma.
+ * 
  * @author Pentolo
  *
  */
 public class DBDataModel {
 
-	public DBDataModel(LinkedList<Account> accounts, LinkedList<Company> companys,
-			LinkedList<Customers_Suppliers> customersSuppliers, LinkedList<Movement> moviments,
-			LinkedList<Product> products) {
+	private LinkedList<Account> accounts;
+	private LinkedList<Company> companys;
+	private LinkedList<Customers_Suppliers> customersSuppliers;
+	private LinkedList<Movement> moviments;
+	private LinkedList<Product> products;
+	private boolean isAccountsModified = false;
+	private boolean isCompanysModified = false;
+	private boolean isCustomersSuppliersModified = false;
+	private boolean isMovimentsModified = false;
+	private boolean isProductsModified = false;
+	
+	public DBDataModel(final LinkedList<Account> accounts, final LinkedList<Company> companys,
+			final LinkedList<Customers_Suppliers> customersSuppliers, final LinkedList<Movement> moviments,
+			final LinkedList<Product> products) {
 		super();
 		this.accounts = accounts;
 		this.companys = companys;
@@ -21,51 +34,6 @@ public class DBDataModel {
 		this.moviments = moviments;
 		this.products = products;
 	}
-	private LinkedList<Account> accounts;
-	private LinkedList<Company> companys;
-	private LinkedList<Customers_Suppliers> customersSuppliers;
-	private LinkedList<Movement> moviments;
-	/**
-	 * @return the isAccountsModified
-	 */
-	public final boolean isAccountsModified() {
-		return isAccountsModified;
-	}
-
-	/**
-	 * @return the isCompanysModified
-	 */
-	public final boolean isCompanysModified() {
-		return isCompanysModified;
-	}
-
-	/**
-	 * @return the isCustomersSuppliersModified
-	 */
-	public final boolean isCustomersSuppliersModified() {
-		return isCustomersSuppliersModified;
-	}
-
-	/**
-	 * @return the isMovimentsModified
-	 */
-	public final boolean isMovimentsModified() {
-		return isMovimentsModified;
-	}
-
-	/**
-	 * @return the isProductsModified
-	 */
-	public final boolean isProductsModified() {
-		return isProductsModified;
-	}
-	private LinkedList<Product> products;
-	
-	private boolean isAccountsModified = false;
-	private boolean isCompanysModified = false;
-	private boolean isCustomersSuppliersModified = false;
-	private boolean isMovimentsModified = false;
-	private boolean isProductsModified = false;
 	
 	/**
 	 * @return the accounts
@@ -73,19 +41,68 @@ public class DBDataModel {
 	public LinkedList<Account> getAccounts() {
 		return accounts;
 	}
+	/**
+	 * @return the companys
+	 */
+	public LinkedList<Company> getCompanys() {
+		return companys;
+	}
+	/**
+	 * @return the customersSuppliers
+	 */
+	public LinkedList<Customers_Suppliers> getCustomersSuppliers() {
+		return customersSuppliers;
+	}
+	/**
+	 * @return the moviments
+	 */
+	public LinkedList<Movement> getMoviments() {
+		return moviments;
+	}
+	/**
+	 * @return the products
+	 */
+	public LinkedList<Product> getProducts() {
+		return products;
+	}
 	
+	/**
+	 * @return the isAccountsModified
+	 */
+	public final boolean isAccountsModified() {
+		return isAccountsModified;
+	}
+	
+	/**
+	 * @return the isCompanysModified
+	 */
+	public final boolean isCompanysModified() {
+		return isCompanysModified;
+	}
+	/**
+	 * @return the isCustomersSuppliersModified
+	 */
+	public final boolean isCustomersSuppliersModified() {
+		return isCustomersSuppliersModified;
+	}
+	/**
+	 * @return the isMovimentsModified
+	 */
+	public final boolean isMovimentsModified() {
+		return isMovimentsModified;
+	}
+	/**
+	 * @return the isProductsModified
+	 */
+	public final boolean isProductsModified() {
+		return isProductsModified;
+	}
 	/**
 	 * @param accounts the accounts to set
 	 */
 	public void setAccounts(LinkedList<Account> accounts) {
 		this.accounts = accounts;
 		 isAccountsModified = true;
-	}
-	/**
-	 * @return the companys
-	 */
-	public LinkedList<Company> getCompanys() {
-		return companys;
 	}
 	/**
 	 * @param companys the companys to set
@@ -95,23 +112,11 @@ public class DBDataModel {
 		isCompanysModified = true;
 	}
 	/**
-	 * @return the customersSuppliers
-	 */
-	public LinkedList<Customers_Suppliers> getCustomersSuppliers() {
-		return customersSuppliers;
-	}
-	/**
 	 * @param customersSuppliers the customersSuppliers to set
 	 */
 	public void setCustomersSuppliers(LinkedList<Customers_Suppliers> customersSuppliers) {
 		this.customersSuppliers = customersSuppliers;
 		isCustomersSuppliersModified = true;
-	}
-	/**
-	 * @return the moviments
-	 */
-	public LinkedList<Movement> getMoviments() {
-		return moviments;
 	}
 	/**
 	 * @param moviments the moviments to set
@@ -121,19 +126,10 @@ public class DBDataModel {
 		isCustomersSuppliersModified = true;
 	}
 	/**
-	 * @return the products
-	 */
-	public LinkedList<Product> getProducts() {
-		return products;
-	}
-	/**
 	 * @param products the products to set
 	 */
 	public void setProducts(LinkedList<Product> products) {
 		this.products = products;
 		isCustomersSuppliersModified = true;
 	}
-	
-	
-
 }
